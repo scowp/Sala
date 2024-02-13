4b841f69b67d4db62b09ba7bb1d6a03e664abcd5
Matheus Campolongo <100418456+scowp@users.noreply.github.com>
2024-02-13 13:48:28 -0300

    corrigido

--- sala.py
+++ sala.py
@@ -13,7 +13,7 @@
 #             dic[i,j]=[[i+3,j+3],[i+4,j+4]]
 #             print(dic)
 #         else:
-#             dic[i,j]=[["NaN","NaN"],["NaN","NaN"]]
+#             dic[i,j]=[["Null","Null"],["Null","Null"]]
 #             print(dic)
 
 
@@ -24,8 +24,8 @@ for i in range(1,5):
             dic[i,j]=[[i+3,j+3],[i+4,j+4]]
             print(dic)
         elif i < 3 and j < 8:
-            dic[i,j]=[[i+3,j+3],["NaN","NaN"]]
+            dic[i,j]=[[i+3,j+3],["Null","Null"]]
             print(dic)
         else:
-            dic[i, j] = [["NaN", "NaN"], ["NaN", "NaN"]]
+            dic[i, j] = [["Null", "Null"], ["Null", "Null"]]
             print(dic)
